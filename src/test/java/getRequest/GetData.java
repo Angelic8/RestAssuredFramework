c46c1c4f8ce8e410c81217e2f0348dc707a0d7c5
belle09 <angeliqueumilda1009@gmail.com>
2021-01-30 13:46:59 +0800

    Fixed issues with jsonpath

--- src/test/java/getRequest/GetData.java
+++ src/test/java/getRequest/GetData.java
@@ -1,36 +1,63 @@
 package getRequest;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.restassured.path.json.JsonPath;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeAll;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetData {
 
-    @Test
-    public void testResponseCode(){
+    @BeforeAll
+    public void setupAPI(){
 
-        Response resp = get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7eb8c4ae0df682ffe32eed392b2fc5e4");
+        baseURI = "http://api.openweathermap.org/data/2.5/find?q=London&appid=7eb8c4ae0df682ffe32eed392b2fc5e4";
+
+    } // end method setupAPI()
+
+    @Test(priority = 1)
+    public void testResponseCode() {
+
+        Response resp = given()
+                .contentType(ContentType.JSON)
+                .param("postId", "2")
+                .when()
+                .get("http://api.openweathermap.org/data/2.5/find?q=London&appid=7eb8c4ae0df682ffe32eed392b2fc5e4")
+                .then()
+                //.assertThat().statusCode(200)
+                //.assertThat().body("list.name[0]", equalTo("London"))
+                //.assertThat().body("list.id[0]", equalTo(2643743))
+                //.assertThat().body("list.main[0].temp", equalTo(282.03f))
+                //.assertThat().body("list.wind[0].speed", equalTo(5.14f))
+                .extract().response();
+
+        Map jsonAsMap = new HashMap<>();
 
         int statusCode = resp.getStatusCode();
         long respTime = resp.getTime();
 
-        System.out.println("Status code: " + statusCode);
-        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertEquals("London", resp.jsonPath().getString("list.name[0]"));
+        Assert.assertEquals(resp.jsonPath().getDouble("list.wind[0].speed"), 5.66);
 
-        System.out.println("Response time: "  + respTime);
+        System.out.println("Response Time: " + respTime);
+        System.out.println(resp.jsonPath().prettyPrint());
 
     } // end method testResponseCode()
-
-    @Test
-    public void testResponseAllData(){
-
-        Response resp = get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7eb8c4ae0df682ffe32eed392b2fc5e4");
-
-        String allData = resp.asString();
-
-        System.out.println("Data is: " + allData);
-
-    } // end method testResponseAllData
 
 } // end method GetData
