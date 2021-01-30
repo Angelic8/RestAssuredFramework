26657378eacb5b804355e0eb419fed336e84468e
belle09 <angeliqueumilda1009@gmail.com>
2021-01-30 17:54:55 +0800

    added arraylist for storing String variables

--- src/test/java/getRequest/GetData.java
+++ src/test/java/getRequest/GetData.java
@@ -11,8 +11,7 @@ import io.restassured.response.ResponseBody;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import io.restassured.path.json.JsonPath;
 
@@ -20,6 +19,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.util.Strings;
 
 public class GetData {
 
@@ -39,8 +39,9 @@ public class GetData {
                 .when()
                 .get("http://api.openweathermap.org/data/2.5/find?q=London&appid=7eb8c4ae0df682ffe32eed392b2fc5e4")
                 .then()
-                //.assertThat().statusCode(200)
-                //.assertThat().body("list.name[0]", equalTo("London"))
+                .assertThat().statusCode(200)
+                .assertThat().body("list.name[0]", equalTo("London"))
+                .assertThat().body("list.sys.country", hasItems("GB", "CA", "US", "US", "US"))
                 //.assertThat().body("list.id[0]", equalTo(2643743))
                 //.assertThat().body("list.main[0].temp", equalTo(282.03f))
                 //.assertThat().body("list.wind[0].speed", equalTo(5.14f))
@@ -50,10 +51,19 @@ public class GetData {
 
         int statusCode = resp.getStatusCode();
         long respTime = resp.getTime();
+        ArrayList<String> listCountry = new ArrayList<String>();
+        listCountry.add("GB");
+        listCountry.add("CA");
+        listCountry.add("US");
+        listCountry.add("US");
+        listCountry.add("US");
+
 
         Assert.assertEquals(200, statusCode);
         Assert.assertEquals("London", resp.jsonPath().getString("list.name[0]"));
-        Assert.assertEquals(resp.jsonPath().getDouble("list.wind[0].speed"), 5.66);
+        Assert.assertEquals(resp.jsonPath().getDouble("list.wind[0].speed"), 9.77);
+        Assert.assertEquals(resp.jsonPath().getString("list.sys.country"),listCountry.toString());
+
 
         System.out.println("Response Time: " + respTime);
         System.out.println(resp.jsonPath().prettyPrint());
